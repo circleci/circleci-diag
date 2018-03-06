5e88dbe2e47c93514ef694f553764f808678b143
Joseph W Becher <jwbecher@drazisil.com>
2018-03-06 09:08:20 -0500

    add a pgk script and try to get basename

---
+++ circleci-diag.pkg.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# This env should have been set by a prior script
+echo "The OS is $($CIRCLECI_DIAG_OS)"
--- circleci-diag.sh
+++ circleci-diag.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -x
 
 # Get OS
-uname -s
+export CIRCLECI_DIAG_OS="$(uname -s)"
+echo "The OS is $($CIRCLECI_DIAG_OS)"
+
+BASE=$(basename $filename .sh)
+
+echo "My name is $BASE"
