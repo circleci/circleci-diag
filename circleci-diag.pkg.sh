d8c86211c4cca41f331a0b7a1e71df14d6b5b454
Joseph W Becher <jwbecher@drazisil.com>
2018-03-06 09:52:20 -0500

    test for os env

--- circleci-diag.pkg.sh
+++ circleci-diag.pkg.sh
@@ -2,3 +2,19 @@
 
 # This env should have been set by a prior script
 echo "The OS is $CIRCLECI_DIAG_OS"
+
+if [ -n $CIRCLECI_DIAG_OS ]
+then
+  echo "The OS is $CIRCLECI_DIAG_OS"
+  if [ $CIRCLECI_DIAG_OS == "Linux" ]
+  then
+    echo "foo"
+    exit
+  elif [ $CIRCLECI_DIAG_OS == "Darwin" ]
+  then
+    echo "bar"
+    exit
+  fi
+else
+  echo -e "Someone forgot to set the OS env\n"
+fi
