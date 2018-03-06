6b31e55705b0f2f064f55caa8218e59bb1b946a5
Joseph W Becher <jwbecher@drazisil.com>
2018-03-06 10:04:40 -0500

    Let's try real listing

--- circleci-diag.pkg.sh
+++ circleci-diag.pkg.sh
@@ -1,19 +1,31 @@
 #!/usr/bin/env bash
 
-# This env should have been set by a prior script
-echo "The OS is $CIRCLECI_DIAG_OS"
 
+
+# This function runs if the OS is Linux
+function pkg_linux() {
+  dpkg -l
+  exit
+}
+
+# This function runs if the OS is Darwin
+function pkg_darwin() {
+  brew list --versions
+  exit
+}
+
+#==========================================
+
+# This env should have been set by a prior script
 if [ -n $CIRCLECI_DIAG_OS ]
 then
   echo "The OS is $CIRCLECI_DIAG_OS"
   if [ $CIRCLECI_DIAG_OS == "Linux" ]
   then
-    echo "foo"
-    exit
+    pkg_linux()
   elif [ $CIRCLECI_DIAG_OS == "Darwin" ]
   then
-    echo "bar"
-    exit
+    pkg_darwin()
   fi
 else
   echo -e "Someone forgot to set the OS env\n"
