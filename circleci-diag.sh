8649509418f9bb99e84068311431a12c3fbf749c
Joseph W Becher <jwbecher@drazisil.com>
2018-03-06 09:40:47 -0500

    Search for modules

--- circleci-diag.sh
+++ circleci-diag.sh
@@ -7,3 +7,9 @@ echo "The OS is $CIRCLECI_DIAG_OS"
 BASE=$(basename ${0##*/} .sh)
 
 echo "My name is $BASE"
+
+echo "I have found the following modules:"
+for f in $BASE.*.sh
+do
+	echo "Located module $f"
+done
