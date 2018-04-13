986580bbb6411e0b0d76095322335b7cfec11eb1
Kyle T <Ampix0@gmail.com>
2018-04-13 16:30:44 -0400

    having an issue running this on windows
    the newly created files have some strange extra character.

---
+++ circleci-diag.status.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# URL_GOOGLE="https://status.cloud.google.com/incidents.json" - Hard to extract
+# URL_AWS="https://status.aws.amazon.com/data.json" -> Hard to extract
+# URL_DOCKER="https://status.docker.com/pages/533c6539221ae15e3f000031/rss" -> Hard to extract
+# URL_GH="https://status.github.com/api/status.json"
+# URL_BB="https://bqlf8qjztdtr.statuspage.io/api/v2/status.json"
+
+echo "Aquiring JSON.sh"
+curl https://raw.githubusercontent.com/dominictarr/JSON.sh/master/JSON.sh -s >> ./JSON.sh
+echo "JSON.sh has been downloaded."
+echo "Create status log directory"
+mkdir -p status_logs
+
+function pkg_statusAWS() {
+  echo "Fetching Amazon status"
+  curl https://status.aws.amazon.com/data.json -s >> ./status_logs/aws.json
+}
